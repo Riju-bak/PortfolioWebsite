59dd295f4f7abcab022917dcd4f47658c0d3ea08
Riju-bak <defaltsith@gmail.com>
2026-04-25 11:35:02 -0400

    Modify Embedded Engineer header msg

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -7,8 +7,8 @@ function Header(props) {
         <div className="rk__header section__padding" id="home">
             <div className="rk__header-content">
                 <h1 className="gradient__text">Hello, I am Rijubak</h1>
-                <p>I'm an Embedded Systems Engineer building cool robots for a Robotics Company</p>
-                <p>The robot on the right is perhaps something I'll build in the future :)</p>
+                <p>I'm an Embedded Systems Engineer at Nex Robotics, where we develop advanced industrial robotic systems.</p>
+                <p>The robot on the right is just there coz it looks cool :)</p>
             </div>
             <div className="rk__header-image">
                 <img alt="scary robot" src={robot} />
